e65c2ee70c1beba21d3558c71781a4449d016e88
sandy <121302642+santhoshAI@users.noreply.github.com>
2024-11-23 13:30:57 +0530

    test file

--- app.py
+++ app.py
@@ -49,4 +49,4 @@ def vehicle_registration():
     return render_template("vehicle_registration.html")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5001, debug=True)
